9af94c36e1717b7c249485921c0c67b16cc1ae7c
castrolimoeiro <65961492+@users.noreply.github.com>
2020-10-17 17:00:39 -0300

    aula 1

--- classes.py
+++ classes.py
@@ -53,3 +53,9 @@ class Pessoa:
 
     def get_ano_nascimento(self):
         return print(self.ano_atual - self.idade)
+
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)
+
--- main.py
+++ main.py
@@ -4,8 +4,13 @@ p1 = Pessoa('David', 31)
 p2 = Pessoa('Noah', 6)
 
 
-p1.falar('POO')
-p2.comer('biscoito')
-print(p1.ano_atual)
-p2.get_ano_nascimento()
+# p1.falar('POO')
+# p2.comer('biscoito')
+# print(p1.ano_atual)
+# p2.get_ano_nascimento()
+# p1.get_ano_nascimento()
+
+p1 = Pessoa.por_ano_nascimento('Simone', 1988)
+print(p1)
+print(p1.nome, p1.idade)
 p1.get_ano_nascimento()
