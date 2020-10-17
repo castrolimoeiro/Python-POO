3da1d7b34dbb117b12172f91926235d43f6522f6
castrolimoeiro <65961492+@users.noreply.github.com>
2020-10-17 16:21:50 -0300

    aula 1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from classes import Pessoas
+from classes import Pessoa
 
-p1 = Pessoas()
-p2 = Pessoas()
+p1 = Pessoa('David', 31)
+p2 = Pessoa('Noah', 6)
 
-p1.nome = 'David'
 
-print(p1.nome)
+p1.falar('POO')
+p2.comer('biscoito')
+print(p1.ano_atual)
+p2.get_ano_nascimento()
+p1.get_ano_nascimento()
